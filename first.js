bfc6521a2f72567ab1f0999e57d08ed86fc84cd7
Prashant <prashantjat622@gmail.com>
2025-01-11 15:30:48 +0530

    seconde  commit

--- first.js
+++ first.js
@@ -1,1 +1,12 @@
 console.log("Hello");
+const button = "Added a button";
+console.log(button);
+
+const login = "Login page added";
+const footer = "Footer added in our website";
+console.log(footer);
+const payment = "Integrated the payment getway";
+console.log(payment);
+
+const upi = "Integrated the upi";
+console.log(upi);
